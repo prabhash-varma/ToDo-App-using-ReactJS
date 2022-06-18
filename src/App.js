740bd1c17fefba17e098bc13755f928af63937b6
Prabhash Varma <varmaprabhash@gmail.com>
2022-06-18 20:16:29 +0530

    Add new file

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+
+  const [task,settask]=useState('');
+  const [tasklist,settasklist]=useState([]);
+
+
+  const addTask = ()=>{
+    settasklist([...tasklist,task]);
+  
+  }
+
+
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+
+      <h2>Add a Task</h2>
+      <textArea cols="60" rows="8" onChange={(event)=>{
+        settask(event.target.value);
+      }}></textArea>
+      <br></br>
+      <button class="btn" onClick={addTask}>Add Task</button>
+
+    
+    {tasklist.map((task)=>{
+      return( 
+        <div class="box">
+          <h4>{task}</h4> 
+        </div>
+      
+      )
+     })}
+  
+    
+    
+    </div>
+  );
+}
+
+export default App;
