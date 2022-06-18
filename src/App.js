2ed4a82a09cf3879d861b4bded6b06099abe053b
Prabhash Varma <varmaprabhash@gmail.com>
2022-06-18 21:39:46 +0530

    Add code

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ function App() {
   return (
     <div className="App">
       <h1>ToDo List</h1>
-
+      <hr></hr>
       <h2>Add a Task</h2>
       <textArea cols="60" rows="8" onChange={(event)=>{
         settask(event.target.value);
       }}></textArea>
       <br></br>
-      <button class="btn" onClick={addTask}>Add Task</button>
+      <button class="btn" onClick={addTask}>Add</button>
 
-    
+    <hr></hr>
     {tasklist.map((task)=>{
       return( 
         <div class="box">
