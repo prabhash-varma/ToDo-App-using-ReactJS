0ae13e4035d8be503a0825b75e34295c8e95abb1
Prabhash Varma <varmaprabhash@gmail.com>
2022-06-19 17:15:06 +0530

    Add code

--- src/App.js
+++ src/App.js
@@ -19,8 +19,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1>ToDo List</h1>
-      <hr></hr>
+     
+      <h1> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSETmar4rq8KLkhHzhxG-6si0x7z6de4PJJFdbIIH7dYF00-ajglV7T1mkreYfCC2PmI1E&usqp=CAU"></img>ToDo List</h1>
+      
+
+      <div id="addbox">
       <h2>Add a Task</h2>
       <textArea id="textbox" cols="60" rows="8" onChange={(event)=>{
         settask(event.target.value);
@@ -28,8 +31,12 @@ function App() {
       <br></br>
       <button class="btn" onClick={addTask}>Add</button>
       <button class="btn" onClick={removeTask}>Clear All</button>
+      <br></br>
+      <br></br>
+      </div>
 
-    <hr></hr>
+
+    
     {tasklist.map((task)=>{
       return( 
         <div class="box">
